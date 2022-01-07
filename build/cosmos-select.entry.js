3d3d58df2bcbdafa37cae964e7e91e1e9349df19
iseekTo <1214046231@qq.com>
2022-01-07 10:43:14 +0800

    Updates

---
+++ build/cosmos-select.entry.js
@@ -0,0 +1,17 @@
+import { r as registerInstance, h } from './index-508f9f6f.js';
+
+const cosmosSelectCss$1 = ".c{color:red}";
+
+const cosmosSelectCss = ".c{color:red}";
+
+const CosmosSelect = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+  }
+  render() {
+    return (h("div", null, h("span", { class: "c" }, "select ddsdsdsdds")));
+  }
+};
+CosmosSelect.style = cosmosSelectCss;
+
+export { CosmosSelect as cosmos_select };
